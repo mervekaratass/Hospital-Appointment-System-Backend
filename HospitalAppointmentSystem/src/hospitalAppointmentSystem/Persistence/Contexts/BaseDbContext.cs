cb1384ceeb1f451aea48fb733d648c48a613f248
mervekaratass <72658817+mervekaratass@users.noreply.github.com>
2024-06-03 16:30:34 +0300

    Add DbSet

--- HospitalAppointmentSystem/src/hospitalAppointmentSystem/Persistence/Contexts/BaseDbContext.cs
+++ HospitalAppointmentSystem/src/hospitalAppointmentSystem/Persistence/Contexts/BaseDbContext.cs
@@ -15,6 +15,17 @@ public class BaseDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Manager> Managers { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Branch> Branches { get; set; }
+    public DbSet<Report> Reports { get; set; }
+
+    public DbSet<Feedback> Feedbacks { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+
     public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration)
         : base(dbContextOptions)
     {
