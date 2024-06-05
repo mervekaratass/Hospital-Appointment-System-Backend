e444dc1f8ac78b8ca16cbd403e98ca0bde99d2d8
Fatma Birel <fatmabrl11@gmail.com>
2024-06-05 16:02:35 +0300

    user validation

--- HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Users/Queries/GetById/GetByIdUserResponse.cs
+++ HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Users/Queries/GetById/GetByIdUserResponse.cs
@@ -7,7 +7,12 @@ public class GetByIdUserResponse : IResponse
     public Guid Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string NationalIdentity { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
     public bool Status { get; set; }
 
     public GetByIdUserResponse()
@@ -15,14 +20,24 @@ public class GetByIdUserResponse : IResponse
         FirstName = string.Empty;
         LastName = string.Empty;
         Email = string.Empty;
+        DateOfBirth = new DateOnly(); // Varsayýlan tarih 0001-01-01 olacaktýr
+        NationalIdentity = string.Empty;
+        Phone = string.Empty;
+        Address = string.Empty;
+        Password = string.Empty;
     }
 
-    public GetByIdUserResponse(Guid id, string firstName, string lastName, string email, bool status)
+    public GetByIdUserResponse(Guid ýd, string firstName, string lastName, DateOnly dateOfBirth, string nationalIdentity, string phone, string address, string email, string password, bool status)
     {
-        Id = id;
+        Id = ýd;
         FirstName = firstName;
         LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        NationalIdentity = nationalIdentity;
+        Phone = phone;
+        Address = address;
         Email = email;
+        Password = password;
         Status = status;
     }
 }
