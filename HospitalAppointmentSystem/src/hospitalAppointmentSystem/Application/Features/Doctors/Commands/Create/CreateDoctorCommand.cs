0e1686a28c55ace712a3fcbcc266a2975dfbfd4e
Fatma Birel <fatmabrl11@gmail.com>
2024-06-05 15:37:30 +0300

    doctor validation

--- HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Doctors/Commands/Create/CreateDoctorCommand.cs
+++ HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Doctors/Commands/Create/CreateDoctorCommand.cs
@@ -23,6 +23,8 @@ public class CreateDoctorCommand : IRequest<CreatedDoctorResponse>, ISecuredRequ
     public string NationalIdentity { get; set; }
     public string Phone { get; set; }
     public string Address { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 
     public string[] Roles => [Admin, Write];
 
