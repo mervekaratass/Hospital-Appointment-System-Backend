2a1243c2634fd63658307aa38464756a66669f5d
Fatma Birel <fatmabrl11@gmail.com>
2024-06-05 15:09:09 +0300

    feedbacks validation

--- HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Feedbacks/Commands/Update/UpdateFeedbackCommandValidator.cs
+++ HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Feedbacks/Commands/Update/UpdateFeedbackCommandValidator.cs
@@ -6,8 +6,10 @@ public class UpdateFeedbackCommandValidator : AbstractValidator<UpdateFeedbackCo
 {
     public UpdateFeedbackCommandValidator()
     {
-        RuleFor(c => c.Id).NotEmpty();
-        RuleFor(c => c.UserID).NotEmpty();
-        RuleFor(c => c.Text).NotEmpty();
+        RuleFor(c => c.Id).NotEmpty().WithMessage("Id alaný boþ olamaz");
+        RuleFor(c => c.UserID).NotEmpty().WithMessage("User Id alaný boþ olamaz");
+        RuleFor(c => c.Text)
+         .NotEmpty().WithMessage("Metin alaný boþ olamaz.")
+         .Length(5, 500).WithMessage("Metin en az 5, en fazla 500 karakter olmalýdýr.");
     }
 }
