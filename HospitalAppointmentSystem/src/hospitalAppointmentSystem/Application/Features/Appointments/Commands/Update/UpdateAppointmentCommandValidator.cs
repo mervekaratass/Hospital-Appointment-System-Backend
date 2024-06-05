94f2e56b487d7cb1254f8ddfd5f1e60d81494cd4
Fatma Birel <fatmabrl11@gmail.com>
2024-06-05 16:05:16 +0300

    appointments validation

--- HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Appointments/Commands/Update/UpdateAppointmentCommandValidator.cs
+++ HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Appointments/Commands/Update/UpdateAppointmentCommandValidator.cs
@@ -6,11 +6,12 @@ public class UpdateAppointmentCommandValidator : AbstractValidator<UpdateAppoint
 {
     public UpdateAppointmentCommandValidator()
     {
-        RuleFor(c => c.Id).NotEmpty();
-        RuleFor(c => c.Date).NotEmpty();
-        RuleFor(c => c.Time).NotEmpty();
-        RuleFor(c => c.Status).NotEmpty();
-        RuleFor(c => c.DoctorID).NotEmpty();
-        RuleFor(c => c.PatientID).NotEmpty();
+        RuleFor(c => c.Id).NotEmpty().WithMessage("Id alaný boþ býrakýlamaz.");
+        RuleFor(c => c.Date).NotEmpty().WithMessage("Tarih alaný boþ býrakýlamaz.");
+        RuleFor(c => c.Time).NotEmpty().WithMessage("Saat alaný boþ býrakýlamaz.");
+        RuleFor(c => c.Status).NotEmpty().WithMessage("Durum alaný boþ býrakýlamaz.");
+        RuleFor(c => c.DoctorID).NotEmpty().WithMessage("Doktor Id alaný boþ býrakýlamaz.");
+        RuleFor(c => c.PatientID).NotEmpty().WithMessage("Hasta Id alaný boþ býrakýlamaz.");
+
     }
 }
