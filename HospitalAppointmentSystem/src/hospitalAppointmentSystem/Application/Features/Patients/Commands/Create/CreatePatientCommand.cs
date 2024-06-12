d8348a1d0d8d0cb8f0988d069f33acfc66561c69
Fatma Birel <fatmabrl11@gmail.com>
2024-06-12 12:02:23 +0300

    doctor & user password hashed

--- HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Patients/Commands/Create/CreatePatientCommand.cs
+++ HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Patients/Commands/Create/CreatePatientCommand.cs
@@ -9,6 +9,8 @@ using NArchitecture.Core.Application.Pipelines.Logging;
 using NArchitecture.Core.Application.Pipelines.Transaction;
 using MediatR;
 using static Application.Features.Patients.Constants.PatientsOperationClaims;
+using NArchitecture.Core.Security.Hashing;
+using System.Numerics;
 
 namespace Application.Features.Patients.Commands.Create;
 
@@ -50,6 +52,14 @@ public class CreatePatientCommand : IRequest<CreatedPatientResponse>, ISecuredRe
         public async Task<CreatedPatientResponse> Handle(CreatePatientCommand request, CancellationToken cancellationToken)
         {
             Patient patient = _mapper.Map<Patient>(request);
+
+            HashingHelper.CreatePasswordHash(
+               request.Password,
+               passwordHash: out byte[] passwordHash,
+               passwordSalt: out byte[] passwordSalt
+           );
+            patient.PasswordHash = passwordHash;
+            patient.PasswordSalt = passwordSalt;
 
             await _patientRepository.AddAsync(patient);
 
