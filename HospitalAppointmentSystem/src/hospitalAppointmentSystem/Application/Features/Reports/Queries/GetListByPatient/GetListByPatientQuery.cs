4ee8fc3b1a095127d0d7e992b44f469b99274537
Sinem Girit <sinemdursun106@gmail.com>
2024-07-04 02:23:27 +0300

    şifre çözdurme

--- HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Reports/Queries/GetListByPatient/GetListByPatientQuery.cs
+++ HospitalAppointmentSystem/src/hospitalAppointmentSystem/Application/Features/Reports/Queries/GetListByPatient/GetListByPatientQuery.cs
@@ -57,7 +57,24 @@ public class GetListByPatientQuery : IRequest<GetListResponse<GetListByPatientDt
            );
 
 
-      
+            for (int i = 0; i < reports.Items.Count; i++)
+            {
+               
+                reports.Items[i].Appointment.Patient.FirstName = CryptoHelper.Decrypt(reports.Items[i].Appointment.Patient.FirstName);
+                reports.Items[i].Appointment.Patient.LastName = CryptoHelper.Decrypt(reports.Items[i].Appointment.Patient.LastName);
+                reports.Items[i].Appointment.Patient.NationalIdentity = CryptoHelper.Decrypt(reports.Items[i].Appointment.Patient.NationalIdentity);
+                reports.Items[i].Appointment.Patient.Email = CryptoHelper.Decrypt(reports.Items[i].Appointment.Patient.Email);
+                reports.Items[i].Appointment.Patient.Phone = CryptoHelper.Decrypt(reports.Items[i].Appointment.Patient.Phone);
+                reports.Items[i].Appointment.Doctor.FirstName = CryptoHelper.Decrypt(reports.Items[i].Appointment.Doctor.FirstName);
+                reports.Items[i].Appointment.Doctor.LastName = CryptoHelper.Decrypt(reports.Items[i].Appointment.Doctor.LastName);
+                reports.Items[i].Appointment.Doctor.Address = CryptoHelper.Decrypt(reports.Items[i].Appointment.Doctor.Address);
+                reports.Items[i].Appointment.Doctor.Email = CryptoHelper.Decrypt(reports.Items[i].Appointment.Doctor.Email);
+                reports.Items[i].Appointment.Doctor.NationalIdentity = CryptoHelper.Decrypt(reports.Items[i].Appointment.Doctor.NationalIdentity);
+                reports.Items[i].Appointment.Doctor.Phone = CryptoHelper.Decrypt(reports.Items[i].Appointment.Doctor.Phone);
+
+
+            }
+
 
             GetListResponse<GetListByPatientDto> response = _mapper.Map<GetListResponse<GetListByPatientDto>>(reports);
             return response;
